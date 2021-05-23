b7ecbf0ad8aacc05ee0d76a3e940dede514c6656
Chandana i k <82880993+Chandanaik@users.noreply.github.com>
2021-05-23 10:34:51 +0530

    Add files via upload
    Life in Weeks Python Code

---
+++ main.py
@@ -0,0 +1,8 @@
+age = int(input("What is your current age?"))
+days=90*365
+weeks=90*52
+months=90*12
+my_days=days-(age*365)
+my_weeks=weeks-(age*52)
+my_months=months-(age*12)
+print(f'you have {my_days} days, {my_weeks} weeks ,{my_months} months  left')
